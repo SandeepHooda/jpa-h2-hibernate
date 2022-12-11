84931d979b1ffd021905c9c60ab35b02a3aebb54
sandeephooda <sonu.hooda@gmail.com>
2022-12-11 09:54:23 -0500

    first commit

---
+++ src/main/java/com/sandeep/jpah2hibernate/JpaH2HibernateApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep.jpah2hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaH2HibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaH2HibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandeep/jpah2hibernate/JpaH2HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.jpah2hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaH2HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
